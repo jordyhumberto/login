bd2d8ea80c26bb0e36c57a2fee9063c35bda7f76
jordy <jordyhvillavicencio@gmail.com>
2018-06-18 13:12:48 -0500

    listo,creo

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 			
 			header("location: welcome.php");
 			} else {
-			$error = "El nombre o contraseña son incorrectos";
+			$error = "El nombre o contrase&ntilde;a son incorrectos";
 		}
 	}
 ?>
@@ -37,16 +37,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>login</title>
+	<link rel="stylesheet" href="estilos.css">
 </head>
 <body>
-    <form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST" autocomplete="off"> 
-		<div><label>Usuario:</label><input id="usuario" name="usuario" type="text" ></div>
-		<br />
-		<div><label>Password:</label><input id="clave" name="clave" type="password"></div>
-		<br />
-		<div><input name="login" type="submit" value="login"></div> 
-	</form> 
-	<br />
-	<div style = "font-size:16px; color:#cc0000;"><?php echo isset($error) ? utf8_decode($error) : '' ; ?></div>
+	<div class=cuerpo>
+		<div class="login">
+			<img class="logo" src="logo.png" alt="logo">
+			<form class="formulario" action="<?php $_SERVER['PHP_SELF']; ?>" method="POST" autocomplete="off"> 
+				<label>Usuario:</label><input id="usuario" name="usuario" type="text" >
+				<label>Clave:</label><input id="clave" name="clave" type="password">
+				<input class="boton" name="login" type="submit" value="login">
+			</form> 
+			<br />
+			<div style = "font-size:16px; color:#cc0000;"><?php echo isset($error) ? utf8_decode($error) : '' ; ?></div>	
+		</div>
+	</div>
 </body>
 </html>
