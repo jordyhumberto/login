66d4c9b8f524c222ebe7b88f32c3b9eeca7150f2
jordy <jordyhvillavicencio@gmail.com>
2018-06-18 13:13:56 -0500

    listo, creo otravez

--- index.php
+++ index.php
@@ -1,28 +1,22 @@
 <?php
 	require('conexion.php');
-	
 	session_start();
-	
 	if(isset($_SESSION["id_usuario"])){
 		header("Location: welcome.php");
 	}
-	
 	if(!empty($_POST))
 	{
 		$usuario = mysqli_real_escape_string($mysqli,$_POST['usuario']);
 		$password = mysqli_real_escape_string($mysqli,$_POST['clave']);
 		$error = '';
-		
 		$sql = "SELECT Id_Usuario,Usuario,Clave,Estado FROM usuarios WHERE Usuario = '$usuario' AND Clave = '$password' AND Estado='01'";
 		//, id_tipo
 		$result=$mysqli->query($sql);
 		$rows = $result->num_rows;
-		
 		if($rows > 0) {
 			$row = $result->fetch_assoc();
 			$_SESSION['id_usuario'] = $row['Id_Usuario'];
 			//$_SESSION['tipo_usuario'] = $row['id_tipo'];
-			
 			header("location: welcome.php");
 			} else {
 			$error = "El nombre o contrase&ntilde;a son incorrectos";
